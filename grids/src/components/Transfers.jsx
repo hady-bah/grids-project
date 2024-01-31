02aa3e0320979d1637c57f6a9c269463a8d4b888
Hady Bah <hadymbah@gmail.com>
2024-01-31 14:31:29 -0500

    Time accuracy to UTC

--- grids/src/components/Transfers.jsx
+++ grids/src/components/Transfers.jsx
@@ -174,11 +174,12 @@ function Transfers() {
 
   const formatDate = (dateTimeString) => {
     const date = new Date(dateTimeString);
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getUTCFullYear();
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    const day = date.getUTCDate().toString().padStart(2, '0');
     return `${month}/${day}/${year}`;
   }
+  
 
   const formatNumber = (number) => {
     // Ensure the input is a valid number
